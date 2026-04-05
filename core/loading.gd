593444173bcb89e868207f32cd1b0f79d9181fd4
theglitchha <glitch1910@gmail.com>
2026-04-05 03:26:24 -0500

    think i fixed the resolution problems...

--- core/loading.gd
+++ core/loading.gd
@@ -8,4 +8,6 @@ func _ready():
 	if OS.has_feature("pc"):
 		get_tree().change_scene_to_packed(desktop_scene)
 		return
+	var s = get_window().content_scale_size
+	get_window().content_scale_size = Vector2i(s.y, s.x)
 	get_tree().change_scene_to_packed(web_scene)
